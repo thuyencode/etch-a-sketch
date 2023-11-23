5f142d45b4f84aad6a31dbdab879c57186899614
thuyencode <thuyencode@proton.me>
2023-11-23 09:32:16 +0700

    Temporarily stop using vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   base: '/etch-a-sketch/',
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
 })
